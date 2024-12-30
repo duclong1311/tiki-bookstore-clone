ea241e11866d1c07dc40f1bb2caa59cbf16cb162
duclong1311 <duclong1311.work@gmail.com>
2024-12-31 01:59:17 +0700

    uninstall react-json-view

--- src/components/order/OrderHistory.jsx
+++ src/components/order/OrderHistory.jsx
@@ -1,7 +1,6 @@
 import { Badge, Descriptions, Divider, Space, Table, Tag } from "antd";
 import { Children, useEffect, useState } from "react";
 import { getOrderHistory } from "../../services/api";
-import ReactJson from 'react-json-view'
 import { formatDate } from "../../services/formatDate";
 import './OrderHistory.scss';
 
